63944f969998883d6410883a486e86db47057f58
denis.pav <denis.pav@okwork.io>
2021-02-27 15:49:31 +0300

    Records pass to props

--- app/javascript/components/Records.js
+++ app/javascript/components/Records.js
@@ -1,10 +1,28 @@
 import React from "react";
 
 export default class Records extends React.Component {
+
   render() {
+    const {data} = this.props
+
     return (
       <div className="Records">
         <h2 className={"title"}>Suppa records</h2>
+        <table className={"table table-bordered"}>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Title</th>
+              <th>Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+
+          </tbody>
+        </table>
+        <div>
+          {console.log(data)}
+        </div>
       </div>
     )
   }
