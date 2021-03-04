77319a2bc409972403e143df6550a1a92f28808e
denis.pav <denis.pav@okwork.io>
2021-03-04 21:36:40 +0300

    Records seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+(1..20).each do |i|
+  Record.create(
+    title: "Record #{i}",
+    date: i.days.ago,
+    amount: i * 10,
+  )
+end
