b4536747c5eeb864ffc44fb6f2dff990a4b6338f
Denis Pav <denis.pav@okwork.io>
2021-11-04 17:35:37 +0300

    Post works but csrf token is missing

--- app/javascript/components/recordForm/index.js
+++ app/javascript/components/recordForm/index.js
@@ -4,10 +4,9 @@ import "./recordForm.scss"
 
 export default class RecordForm extends React.Component {
   state = {
-    title: '',
-    date: '',
-    amount: '',
-    valid: false,
+    title: '1',
+    date: '2',
+    amount: '3',
   }
 
   makeChanger = key => e => {
@@ -18,16 +17,28 @@ export default class RecordForm extends React.Component {
     return this.state.title && this.state.date && this.state.amount
   }
 
+  handleSubmit = e => {
+    e.preventDefault()
+    console.log(this.state)
+    fetch("",
+      {
+        method: "POST",
+        headers: { "Contebt-Type": "application/json" },
+        body: this.state
+      }
+      ).then(response => response.json)
+  }
+
   render () {
     return(
-      <form className='form-inline record-form'>
+      <form className='form-inline record-form' onSubmit={this.handleSubmit}>
         <div className='form-group'>
           <input
             className='form-control'
             type="text"
             placeholder="Date"
             name="Date"
-            value={this.date}
+            value={this.state.date}
             onChange={this.makeChanger("date")}
           />
           <input
@@ -35,7 +46,7 @@ export default class RecordForm extends React.Component {
             className="form-control"
             placeholder="Title"
             name="Title"
-            value={this.title}
+            value={this.state.title}
             onChange={this.makeChanger("title")}
           />
           <input
@@ -43,7 +54,7 @@ export default class RecordForm extends React.Component {
             className="form-control"
             placeholder="Amount"
             nama="Amount"
-            value={this.amount}
+            value={this.state.amount}
             onChange={this.makeChanger("amount")}
           />
         </div>
