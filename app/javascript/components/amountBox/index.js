0aeba0833accaa04103af111015114b71e7d7af5
denis.pav <denis.pav@okwork.io>
2021-11-06 13:38:02 +0300

    Panel

--- app/javascript/components/amountBox/index.js
+++ app/javascript/components/amountBox/index.js
@@ -1,1 +1,21 @@
 import React from "react"
+
+class AmountBox extends React.Component {
+  render () {
+
+    const { type, text, amount } = this.props
+
+    return(
+      <div className="col-md-4 amount-box">
+        <div className={`amount-box__panel panel-${type}`}>
+          <div className="amount-box__panel-heading">
+            {text}
+          </div>
+          <div className="amount-box__panel-body">
+            {amount}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
