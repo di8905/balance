3c6718c2ba1277e17958d39e0e6b7632958a69d8
denis.pav <denis.pav@okwork.io>
2021-11-07 12:26:12 +0300

    Form setup basic

--- app/javascript/components/Record/index.js
+++ app/javascript/components/Record/index.js
@@ -19,29 +19,82 @@ export default class Record extends React.Component {
     this.setState(prevState => ({ edit: !prevState.edit }))
   }
 
+  handleEdit = () => {
+    alert("aaa")
+  }
+
+  recordForm = () => {
+    return(
+      <tr>
+        <td>
+          <input
+            className="form-control"
+            type="text"
+            defaultValue={this.props.record.date}
+          />
+        </td>
+        <td>
+          <input
+            className="form-control"
+            type="text"
+            defaultValue={this.props.record.title}
+          />
+        </td>
+        <td>
+          <input
+            className="form-control"
+            type="text"
+            defaultValue={this.props.record.amount}
+          />
+        </td>
+        <td>
+          <a
+            className="btn btn-default"
+            onclick={this.handleEdit}
+          >
+            Update
+          </a>
+          <a
+            className="btn btn-danger"
+            onClick={this.handleEditToggle}
+          >
+            Cancel
+          </a>
+        </td>
+      </tr>
+    )
+
+  }
+
+  recordRow = () => {
+    return(
+      <tr>
+        <td>{record.date}</td>
+        <td>{record.title}</td>
+        <td>{record.amount}</td>
+        <td>
+          <a
+            className="btn btn-danger"
+            onClick={this.handleDelete}
+          >
+            Delete
+          </a>
+          <a
+            className="btn btn-info"
+            onClick={this.handleEditToggle}
+          >
+            Edit
+          </a>
+        </td>
+      </tr>
+    )
+  }
+
   render () {
     const { record } = this.props
 
     return (
-        <tr>
-          <td>{record.date}</td>
-          <td>{record.title}</td>
-          <td>{record.amount}</td>
-          <td>
-            <a
-              className="btn btn-danger"
-              onClick={this.handleDelete}
-            >
-              Delete
-            </a>
-            <a
-              className="btn btn-info"
-              onClick={this.handleEditToggle}
-            >
-              Edit
-            </a>
-          </td>
-        </tr>
+
     )
 
   }
